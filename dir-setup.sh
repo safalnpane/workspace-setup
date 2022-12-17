c49352e4ac624f7dbb1da09a90843e9569dd7ce5
Safal Neupane <safal.npane@gmail.com>
2022-12-17 15:07:04 +1100

    Initial dir-setup script

---
+++ dir-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Create my usual worflow directories for various languages
+mkdir -p workspace/{python,js}/github.com/{safalnpane,Devs-Education}
+mkdir -p workspace/go/{bin,src}
+mkdir -p workspace/go/src/github.com/{safalnpane,Devs-Education}
